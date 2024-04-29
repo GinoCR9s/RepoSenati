5a46616699f66198ad08fee54caf614363f18d58
JACKIE CHAN <ginochipana8@gmail.com>
2024-04-29 11:01:44 -0500

    Se agrego un espacio para una calculadora

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ a=5
 b=10
 print("Hola mundo")
 print("La suma es: ", a+b)
+print("La resta es: ", a-b)
+print("*****************************")
+print("********CALCULADORA**********")
+print("*****************************")
